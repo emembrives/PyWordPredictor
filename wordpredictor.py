695e7421ca485354aa6a7adfbd02aa5b42d6491f
Etienne Membrives <etienne@membrives.fr>
2010-03-29 11:45:54 -0700

    XML-RPC logic

--- wordpredictor.py
+++ wordpredictor.py
@@ -15,3 +15,23 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 # Implement here the XML-RPC server
+
+from SimpleXMLRPCServer import SimpleXMLRPCServer
+from SimpleXMLRPCServer import SimpleXMLRPCRequestHandler
+import wordmodel
+
+predictor=wordmodel.WordModel([])
+
+# Restrict to a particular path.
+class RequestHandler(SimpleXMLRPCRequestHandler):
+    rpc_paths = ('/RPC2',)
+
+# Create server
+server = SimpleXMLRPCServer(("localhost", 8000),
+                            requestHandler=RequestHandler)
+server.register_introspection_functions()
+
+server.register_instance(predictor)
+
+# Run the server's main loop
+server.serve_forever()
